4ec19a2cacbee4f10562d639d2fcf7f11bc9f14f
Ethan Louie <eklouie@uci.edu>
2020-10-29 06:37:36 -0700

    bot posts public IP after starting

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import discord
 from requests import get
 
 ip = get('https://api.ipify.org').text
-print('My public IP address is: {}'.format(ip))
 
 file = open('token.txt', 'r')
 token = file.read().strip()
@@ -10,7 +9,9 @@ file.close()
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        channel = client.get_channel(770213216449069067)
+        await channel.send(f'My public IP address is: {ip}')
+        print(f'Logged in as {self.user} and posted public IP')
 
     async def on_message(self, message):
         # don't respond to ourselves
