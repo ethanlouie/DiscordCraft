e6216fa1d289543c4cc9cfb5402c1b7b719fde82
Christopher Haley <cjhaley@uci.edu>
2020-11-27 20:12:01 -0800

    server statistics added

--- server_stats.py
+++ server_stats.py
@@ -7,8 +7,10 @@ def get_players(host,port):
             if len(players) == 0:
                 return "no one online"
             elif len(players) > 0:
-                return "players online:".join(str(player)+"\n" for player in players)
+                result = "Who's online:\n"
+                return result +"".join(str(player)+"\n" for player in players)
     except ConnectionError:
         return "server offline or otherwise unable to connect :("
 
-
+#test it out wit dis
+print(get_players("68.109.198.55",25565))
